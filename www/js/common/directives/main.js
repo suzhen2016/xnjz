c49a2e023245b91ad562c8c997526db95146690f
苏镇 <1461685273@qq.com>
2018-09-08 11:37:45 +0800

    修改兼容处理

--- www/js/common/directives/main.js
+++ www/js/common/directives/main.js
@@ -437,45 +437,6 @@ angular
             ionicToast.alert('获取电话失败，稍后再试')
           }
         }
-<<<<<<< HEAD
-=======
-    }])
-    /**
-     * 打电话指令
-     * */
-    .directive('xnCall', function (iAlert, AccountService, $rootScope, ionicToast, Storage) {
-        return {
-            restrict: 'EAC',
-            replace: true,
-            scope: {
-
-            },
-            link: function ($scope, element, attributes) {
-                var vm = $scope.vm = this;
-                vm.query = {};
-                vm.query.isFalg = true;
-                vm.telPhone = function () {
-                    console.log(22)
-                    //vm.getPhone();
-                    var query = {
-                        type: 'radio'
-                    };
-                    var obj = {
-                        templateUrl: '<ul class="list genderRadio">' +
-                            '<li class="item item-checkbox item-checkbox1 item-icon-right" ng-repeat="item in vm.query.iphoneList track by $index">' +
-                            '<label class="checkbox" style="width:250px;">' +
-                            '<input type="radio" ng-value="item.cos_phone" ng-model="vm.query.phone">' +
-                            '</label>' +
-                            '{{item.cos_phone}}&nbsp' +
-                            "</li>" +
-                            '</ul>',
-                        btn: '拨号'
-                    };
-
-                    if (vm.query.iphoneList) {
-                        //iAlert.confirm('', '13796003244', vm.success, vm.err, obj)
-                        iAlert.tPopup($scope, obj, query, vm.success, vm.err);
->>>>>>> 8b0c5b038174469ec9a596b16a6d9df57b4801f7
 
         vm.getPhone = function() {
           if (!vm.query.isFalg) {
@@ -507,7 +468,6 @@ angular
             })
         }
 
-<<<<<<< HEAD
         //成功打电话
         vm.success = function() {
           window.plugins.CallNumber.callNumber(
@@ -629,186 +589,6 @@ angular
                     } else {
                       ionicToast.alert('上传失败!')
                       $ionicLoading.hide()
-=======
-                vm.getPhone = function () {
-                    if (!vm.query.isFalg) {
-                        return false;
-                    }
-                    vm.query.isFalg = false;
-                    AccountService.xnGetPhonekf().then(function (res) {
-                        if (res.status == 200) {
-                            vm.query.iphoneList = res.data;
-
-                            if (_.size(vm.query.iphoneList) > 0) {
-                                vm.query.isFalg = true;
-                                vm.telPhone();
-                            } else {
-                                ionicToast.alert('客服电话更新中，请稍后再访问')
-                            }
-
-                        } else {
-                            ionicToast.alert('获取电话失败，请稍后再访问')
-                        }
-                    }, function () {
-                        ionicToast.alert('网络延迟，请稍后再访问')
-                    }).finally(function () {
-                        vm.query.isFalg = true;
-                    })
-                }
-
-                //成功打电话
-                vm.success = function () {
-
-                    window.plugins.CallNumber.callNumber(function onSuccess(res) {
-                        $log.debug('Succcess:call number' + res)
-                    },
-                        function onError(res) {
-                            $log.debug('Error:call number' + res)
-                        },
-                        vm.query.phone, true);
-                }
-
-                vm.err = function () {
-                }
-                element.on('click', vm.getPhone);
-
-            }
-        }
-    })
-
-    /**
-     *
-     * 上传图片组件
-     */
-    .directive('tradeImgTool', function (iAlert, XnAlert, FileUpload, fileReader, $rootScope, AccountService, $ionicHistory, $log, $http, ionicToast, ENV, $ionicLoading,Storage) {
-
-        return {
-            restrict: 'ECA',
-            replace: true,
-            scope: {
-                fun:'='
-            },
-            // templateUrl: 'js/common/directives/templates/company_page.html',
-            link: function ($scope, element, attr) {
-                element.on('click', function () {
-                    alert('请选择图片上传方式')
-                    $scope.upLoadOrderImg();
-                });
-                //获取input中files 信息
-                $scope.getUploadPic = function (e) {
-                    $scope.file = e;
-                    //console.log($scope.file);
-                    $scope.getFile()
-                };
-
-                $scope.getFile = function () {
-
-                    fileReader.readAsDataUrl($scope.file[0], $scope).then(function (result) {
-                        if (result) {
-                            $scope.previewImageSrc = result;
-                            // $scope.imgUrl.push(result)
-                        }
-                    })
-                };
-
-                var _upimg = function (file, _url, $http) {
-                    var c = new FormData();
-                    c.append('File', file);
-                    c.append('type', 'file')
-                    console.log(c,file)
-                    // 上传图片
-                    return $http({
-                        method: 'POST',
-                        url: _url,
-                        data: c,
-                        headers: {
-                            "Content-Type": undefined
-                        },
-                        transformRequest: angular.identity
-                    })
-                };
-                //上传图片方法
-                $scope.upLoadOrderImg = function () {
-                    $scope.previewImageSrc = '';
-                    if (ionic.Platform.isWebView()) { //APP端
-                        var opt = {
-                            // params: {
-                            //     'type': 'tou_xiang'
-                            // },
-                            //url: ENV.api.trade + 'file/img_upload',
-                            url: 'http://www.zgxnjz.cn/index.php/Home/UserPho/ImgUpLoadFile',
-                            headers: {
-                                'x-access-token': $rootScope.user.token,
-                                "Content-Type":undefined
-                            }
-                        }
-
-                        FileUpload.upload('resizeImg', opt, function (res) {
-                        $ionicLoading.show({
-                                             template: "上传中..."
-                                         })
-                            res.then(function (json) {
-                                $log.debug('上传图片的回调', json)
-                                var result = JSON.parse(json.response);
-                               
-                                if (result.status == "200") {
-                                    $ionicLoading.hide();
-                                    ionicToast.alert('上传成功!');
-
-                                    $scope.fun.user_pho = result.data.img_url;
-                                    Storage.set('userInfo',$scope.fun)
-                                    console.log($scope.fun)
-
-                                } else {
-                                    ionicToast.alert('上传失败!');
-                                    $ionicLoading.hide();
-                                }
-                            }, function (err) {
-                                console.log(err)
-                                $ionicLoading.hide();
-                                ionicToast.alert('上传失败!');
-                            }, function (progress) {
-
-                            }).finally(function () {
-                                $ionicLoading.hide();
-
-                            });
-                        })
-                    } else {   //pc端
-
-                        var data = {
-                            type: 'file'
-                        }
-                        var obj = {
-                            templateUrl: 'js/common/template/popupRadio.html',
-                            title: '上传头像'
-                        }
-                        XnAlert.tPopup($scope, obj, data, function (res) {
-
-                            if (res) {
-                                if (!$scope.file) {
-                                    ionicToast.alert('选择图片失败,再次上传');
-                                    return false;
-                                }
-                                // var _url = ENV.api.trade + 'file/img_upload';
-                                var _url = 'http://www.zgxnjz.cn/index.php/Home/UserPho/ImgUpLoadFile';
-                                _upimg($scope.file[0], _url, $http).success(function (data) {
-
-                                    if (data.status == "200") {
-                                        ionicToast.alert('更换头像成功')
-                                      
-                                        $scope.fun.user_pho = data.data.img_url;
-                                        Storage.set('userInfo',$scope.fun)
-                                        console.log($scope.fun)
-                                        
-                                    } else {
-                                        ionicToast.alert('图片上传失败,请稍后重试')
-                                    }
-                                })
-                            }
-
-                        })
->>>>>>> 8b0c5b038174469ec9a596b16a6d9df57b4801f7
                     }
                   },
                   function(err) {
@@ -856,11 +636,6 @@ angular
             })
           }
         }
-<<<<<<< HEAD
       }
     }
   })
-=======
-    })
-   
->>>>>>> 8b0c5b038174469ec9a596b16a6d9df57b4801f7
--- www/js/commonPage/controller.js
+++ www/js/commonPage/controller.js
@@ -79,7 +79,6 @@ angular
             function() {
               ionicToast.alert('网络延迟，请稍后再试')
             }
-<<<<<<< HEAD
           )
           .finally(function() {
             vm.getCar()
@@ -101,32 +100,6 @@ angular
                   value.isOrder = true
                   value.isDele = false
                   vm.query.sum += value.goods_total_price - 0
-=======
-
-            //查询
-            vm.getCar = function () {
-
-                FirstService.getCarDetails().then(function (res) {
-                    if (res.status == 200) {
-                        vm.query.sum = 0;
-                        vm.carList = res.data;
-                        vm.carList.forEach(function (value, key) {
-                            value.isOrder = true;
-                            value.isDele = false;
-                            vm.query.sum += (value.goods_total_price - 0)
-
-                        })
-                        vm.isSeletAll();
-                        vm.isLoading = false;
-                        $log.debug('加入购物车数', vm.query.sum,vm.carList, res);
-                    } else {
-
-                    }
-                }, function () {
-
-                }).finally(function () {
-                    vm.isLoading = false;
->>>>>>> 8b0c5b038174469ec9a596b16a6d9df57b4801f7
                 })
                 vm.isSeletAll()
                 vm.isLoading = false
@@ -314,7 +287,6 @@ angular
               ionicToast.alert('网络延迟，请稍后在试')
               $ionicHistory.goBack()
             }
-<<<<<<< HEAD
           )
           .finally(function() {
             if (!vm.dele.null) {
@@ -340,41 +312,6 @@ angular
         $log.debug(vm.order, vm.dele.address)
         if (!vm.dele.address) {
           vm.getAddres()
-=======
-
-            //检测是否选中
-            vm.isSeletAll = function () {
-                if (vm.isEdit == true) {
-                    var sum = 0
-                    vm.isAllSelect = _.size(_.filter(vm.carList, function (i) {
-                        if (i.isOrder == true) {
-                            sum += (i.goods_total_price - 0)
-                            return i;
-                        }
-                    }));
-                    vm.query.sum = sum;
-                    $log.debug('选中', vm.isAllSelect,sum,vm.query.sum);
-                } else {
-                    vm.isAlldele = _.size(_.filter(vm.carList, function (i) {
-                        if (i.isDele == false) {
-                            return i;
-                        }
-                    })) == 0;
-                    $log.debug('选删', vm.isAlldele);
-                }
-
-            }
-
-            vm.goBack = function () {
-                $ionicNavBarDelegate.back();
-                $ionicViewSwitcher.nextDirection("back")
-            }
-
-            $scope.$on("$ionicView.beforeEnter", function () {
-                vm.isEdit = true
-                vm.getCar();
-            })
->>>>>>> 8b0c5b038174469ec9a596b16a6d9df57b4801f7
         }
       }
 
@@ -488,7 +425,6 @@ angular
             function() {
               $state.go('payrelsult', { type: 1 })
             }
-<<<<<<< HEAD
           )
           .finally(function() {})
       }
@@ -519,7 +455,7 @@ angular
           obj.out_trade_no = vm.query.trade_code
           obj.total_fee = vm.query.total_price
 
-          FirstService.getweixinPayString(obj).then(function(res) {
+          FirstService.WeiPaySuccessOrder(obj).then(function(res) {
             if (res.status == 200) {
               var params = {
                 partnerid: res.data.PartnerId, // merchant id
@@ -534,13 +470,11 @@ angular
               Wechat.sendPaymentRequest(
                 params,
                 function() {
-                  ionicToast.alert('订单支付成功')
+                  //ionicToast.alert('订单支付成功')
                   //$state.go('payrelsult', { 'type': 3 })
-                  vm.WeiPaySuccessOrder(arr)
+                  vm.alipaySuccessOrder(arr)
                 },
                 function(reason) {
-                  ionicToast.alert('订单支付失败')
-                  alert(JSON.stringify(reason))
                   $state.go('payrelsult', { type: 1 })
                 }
               )
@@ -586,216 +520,6 @@ angular
                 }
               )
             } else {
-=======
-            vm.pinkUp = function () {
-                $log.debug(vm.dele.obj)
-                if (!vm.dele.obj.cgn_id) {
-                    ionicToast.alert('请选择你的地址')
-                    return false;
-                }
-                $ionicLoading.show({
-                    template: '订单在提交中...'
-                });
-                FirstService.pickOrder(vm.dele.obj).then(function (res) {
-                    if (res.status == 200) {
-                        $log.debug('提交订单', res);
-                        //xnData.remove('pinkUp')
-                        xnData.set(res.data, 'pinkUp');
-                        if (res.data.order_total_price == vm.order[0].sum_all) {
-
-                        }
-                        $state.go('pinkUp')
-
-                    } else {
-                        ionicToast.alert('提交失败，请稍后在试');
-                    }
-                }, function () {
-                    $ionicLoading.hide()
-                    ionicToast.alert('网络延迟，请稍后在试');
-                    $ionicHistory.goBack();
-                }).finally(function () {
-                    $ionicLoading.hide()
-                })
-
-            }
-
-            $scope.$on("$ionicView.beforeEnter", function () {
-                vm.dele.null = false;
-                vm.init();
-            })
-        }
-    ])
-
-    .controller('pink_up_ctrl', ['$stateParams', '$state', '$log', '$scope', '$ionicLoading'
-        , 'ionicToast', '$ionicHistory', '$ionicViewSwitcher', '$ionicNavBarDelegate', 'xnData', 'FirstService',
-        function ($stateParams, $state, $log, $scope, $ionicLoading, ionicToast
-            , $ionicHistory, $ionicViewSwitcher, $ionicNavBarDelegate, xnData, FirstService) {
-            var vm = $scope.vm = this;
-            vm.paytype = 'apli'//默认支付宝支付       
-            vm.order = {};
-            vm.query = {};
-
-            vm.init = function () {
-                vm.order = {};
-                vm.order.data = xnData.get('pinkUp');
-               
-                vm.times = new Date().getTime();
-                vm.query.goods_name = '西南家政商品';
-                vm.query.total_price = vm.order.data.order_total_price;
-                if (vm.order.data.orderRetInfo.length > 1) {
-                    vm.query.trade_code = vm.order.data.orderRetInfo[0].order_number;
-                } else {
-                    vm.query.trade_code = 'ES' + vm.times;
-
-                }
-
-                $log.debug("订单详情", vm.times, vm.order.data);
-            }
-
-            vm.goBack = function () {
-                $ionicNavBarDelegate.back();
-                $ionicViewSwitcher.nextDirection("back")
-            }
-            //成功的回调
-            vm.alipaySuccessOrder = function (arr) {
-                //订单的ids
-                FirstService.clientNotifyUrl(arr).then(function (res) {
-                    if (res.status == 200) {
-                        $state.go('payrelsult', { 'type': 3 })
-                    } else {
-                        $state.go('payrelsult', { 'type': 1 })
-                    }
-                }, function () {
-                    $state.go('payrelsult', { 'type': 1 })
-                }).finally(function () {
-
-                })
-            }
-            //支付
-            vm.alipayXn = function () {
-                xnData.remove('pinkUp');
-                if (vm.paytype == 'weixin') {
-
-                    var obj = {}
-                    obj.body = '家政服务超市商品';
-                    obj.out_trade_no = vm.query.trade_code;
-                    obj.total_fee = vm.query.total_price;
-
-                    FirstService.getweixinPayString(obj).then(function (res) {
-                       
-                        if (res.status == 200) {
-
-                            var params = {
-                                partnerid: res.data.PartnerId, // merchant id
-                                prepayid: res.data.PrePayId, // prepay id
-                                noncestr: res.data.NonceStr, // nonce
-                                timestamp: res.data.Timestamp, // timestamp
-                                sign: res.data.Sign, // signed string
-                                appid:res.data.AppId,
-                                package:res.data.Package,
-                            };
-                            var arr = _.pluck(vm.order.data.orderRetInfo, 'order_number')
-                            Wechat.sendPaymentRequest(params, function () {
-                                //ionicToast.alert('订单支付成功')
-                                //$state.go('payrelsult', { 'type': 3 })
-                                vm.alipaySuccessOrder(arr)
-                            }, function (reason) {
-                                $state.go('payrelsult', { 'type': 1 })
-                            });
-                        } else {
-
-                        }
-
-                    })
-                } else {
-                    //支付宝
-                    var arr = _.pluck(vm.order.data.orderRetInfo, 'order_number')
-                    FirstService.getAlipayString(vm.query).then(function (res) {
-                        console.log(res);
-                        if (res.status == 200 && res.param) {
-                            cordova.plugins.alipay.payment(res.param, function success(e) {
-                                if (e) {
-                                    if (e.resultStatus == 9000) {
-                                        //ionicToast.alert('订单支付成功')
-                                        vm.alipaySuccessOrder(arr)
-
-                                    } else if (e.resultStatus == 8000) {
-                                        //支付中;
-                                        ionicToast.alert('订单支付中，请求订单列表查询订单状态,如果已扣款请联系客服')
-                                        $state.go('tab.order', { 'status': 1 })
-                                    }
-                                }
-                            }, function error(e) {
-                                if (e) {
-                                    //alert(JSON.stringify(e));
-                                    if (e.resultStatus == 4000) {
-                                        //ionicToast.alert('订单支付失败')
-                                        $state.go('payrelsult', { 'type': 1 })
-                                    } else if (e.resultStatus == 6001) {
-                                        ionicToast.alert('您取消支付了');
-                                        $state.go('tab.order', { 'status': 1 })
-                                    } else if (e.resultStatus == 6002) {
-                                        ionicToast.alert('网络原因支付失败');
-                                        $state.go('payrelsult', { 'type': 1 })
-                                    }
-                                }
-                            });
-                        } else {
-
-                        }
-
-                    })
-
-                }
-
-            };
-
-             $scope.$on("$ionicView.beforeEnter", function () {
-                vm.paytype = 'apli'//默认支付宝支付       
-                vm.order = {};
-                vm.order.data = {};
-                vm.query = {};
-                vm.init();
-            })    
-
-        }
-    ])
-
-    .controller('select_address_ctrl', ['$stateParams', '$log', '$scope', '$ionicLoading',
-        'ionicToast', '$ionicHistory', '$ionicViewSwitcher', '$ionicNavBarDelegate', 'FirstService', 'xnData',
-        function ($stateParams, $log, $scope, $ionicLoading, ionicToast, $ionicHistory, $ionicViewSwitcher, $ionicNavBarDelegate, FirstService, xnData) {
-            var vm = $scope.vm = this;
-            $log.debug("地址选择");
-            vm.dele = {};
-            vm.dele.isError = false;
-            vm.address_data = [];
-            vm.dele.isLoading = true;
-            vm.init = function () {
-                vm.address_data = [];
-                FirstService.getAddressList().then(function (res) {
-                    if (res.status == 200) {
-
-                        vm.dele.isError = false;
-                        vm.address_data = res.data;
-                        $log.debug('地址列表', res.data);
-                    } else {
-                        vm.address_data = [];
-
-                        vm.dele.isError = true;
-                    }
-                }, function () {
-                    vm.address_data = [];
-
-                    vm.dele.isError = true;
-                }).finally(function () {
-                    vm.dele.isLoading = false;
-                })
-            }
-            vm.init();
-            vm.selectAddress = function (item) {
-                xnData.set(item, "address");
-                $ionicHistory.goBack();
->>>>>>> 8b0c5b038174469ec9a596b16a6d9df57b4801f7
             }
           })
         }
@@ -935,7 +659,6 @@ angular
           vm.query.text = '支付失败'
           vm.query.ui = 'tab.order({status:1})'
         }
-<<<<<<< HEAD
         timeStop()
       }
       $scope.$on('$ionicView.beforeEnter', function() {
@@ -943,59 +666,3 @@ angular
       })
     }
   ])
-=======
-    ])
-
-    //订单支付结果
-    .controller('payrelsut_ctr', ['$stateParams', '$log', '$scope', '$ionicLoading', '$interval', '$state',
-        'ionicToast', '$ionicHistory', '$ionicViewSwitcher', '$ionicNavBarDelegate', 'FirstService', 'xnData',
-        function ($stateParams, $log, $scope, $ionicLoading, $interval, $state, ionicToast, $ionicHistory, $ionicViewSwitcher, $ionicNavBarDelegate, FirstService, xnData) {
-            var vm = $scope.vm = this;
-            vm.query = {};
-            vm.query.type = $stateParams.type;
-            vm.timer = '';
-            function timeStop() {
-                vm.query.num = 4;
-                vm.timer = $interval(function () {
-
-                    if (vm.query.num == 0) {
-                        $interval.cancel(vm.timer);
-                        vm.timer = null;
-                        console.log(vm.query.num)
-                        $state.go('tab.order', { status: vm.query.type })
-                    } else {
-                        vm.query.num--;
-                        console.log(vm.query.num)
-
-                    }
-                }, 1000)
-            }
-
-            vm.init = function () {
-                if (vm.query.type==3) {
-                    vm.query.img = 'img/xn/pay_success.png';
-                    vm.query.text = '支付成功';
-                    vm.query.ui = 'tab.order({status:2})'
-                } else {
-
-                    vm.query.img = 'img/xn/pay_err.png';
-                    vm.query.text = '支付失败';
-                    vm.query.ui = 'tab.order({status:1})'
-                }
-                timeStop();
-
-            }
-            $scope.$on("$ionicView.beforeEnter", function () {
-
-                vm.init();
-            })
-        }
-    ])
-
-
-
-<<<<<<< HEAD
-=======
- 
->>>>>>> ca47c626bc072ba56563f052d7e0829b7fd4bd57
->>>>>>> 8b0c5b038174469ec9a596b16a6d9df57b4801f7
